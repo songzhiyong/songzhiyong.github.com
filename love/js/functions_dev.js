1399ee9373e4f5410ae8b973cbf2aa25ec1daff5
jerome <songzhiyong1121@gmail.com>
2013-08-04 23:03:14 +0800

    Site updated at 2013-08-04 15:03:14 UTC

--- love/js/functions_dev.js
+++ love/js/functions_dev.js
@@ -99,16 +99,16 @@ function timeElapse(date){
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
-		hours = "0" + hours;
+		hours = "0"+ hours;
 	}
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
-		minutes = "0" + minutes;
+		minutes = "0"+ minutes;
 	}
 	seconds = seconds % 60;
 	if (seconds < 10) {
-		seconds = "0" + seconds;
+		seconds = "0"+ seconds;
 	}
 	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
 	$("#elapseClock").html(result);
