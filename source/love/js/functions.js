c413a51ea67f434d2993a1ce47dbbcd9c4b055b9
jerome <songzhiyong1121@gmail.com>
2013-08-09 23:33:19 +0800

    love u

--- source/love/js/functions.js
+++ source/love/js/functions.js
@@ -122,7 +122,7 @@ function adjustWordsPosition() {
 }
 
 function adjustCodePosition() {
-	$("#code").css("margin-top", ($("#garden").height() - $("#code").height()) / 2)
+	$("#code").css("margin-top", 30)
 }
 
 function showLoveU() {
